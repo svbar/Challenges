b342a9d1cb2bbfa67e0adf8a925b9f01cfdcd19c
svbar <barhate.sachin@gmail.com>
2020-05-11 18:10:07 +0100

    Create variables.tf

---
+++ Challenge1/variables.tf
@@ -0,0 +1,20 @@
+  
+variable "secret_key" {
+    
+    description = "The AWS secret key."
+}
+
+variable "access_key" {
+
+    description = "The AWS access key."
+}
+
+variable "db_password" {
+
+    description = "Mysql DB password."
+}
+
+variable "db_user" {
+
+    description = "Mysql DB user."
+}
